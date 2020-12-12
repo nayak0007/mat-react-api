f73c900ebd68e63b24bf96fb7795f1b2c5c5d602
Dipesh Nayak <dipeshn@spotify.com>
2020-12-12 21:42:20 +0530

    Post api

---
+++ src/main/java/com/matreact/matreact/model/PostRequest.java
@@ -0,0 +1,19 @@
+package com.matreact.matreact.model;
+
+public class PostRequest {
+	int id;
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	String name;
+
+}
